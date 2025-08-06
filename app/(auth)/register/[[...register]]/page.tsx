68e11da2ba1fc0224d2f9f47295aee741c45703c
Zeeshan <mzh881061@gmail.com>
2025-08-06 13:59:16 +0530

    implement register page

--- app/(auth)/register/[[...register]]/page.tsx
+++ app/(auth)/register/[[...register]]/page.tsx
@@ -1,11 +1,18 @@
+import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
-
-const RegisterPage = () => {
-    return(
-        <>
-        <h1>Register Page</h1>
-        </>
-    )
+export default function RegisterPage() {
+    return (
+        <main className="flex flex-col items-center p-5 gap-10 animate-fade-in">
+             <Image
+                src='/assets/logo.svg'
+                width={100}
+                height={100}
+                alt="Logo"
+            />
+            <div className="mt-3">
+                <SignUp />
+            </div>
+        </main>
+)
 }
-
-export default RegisterPage;
